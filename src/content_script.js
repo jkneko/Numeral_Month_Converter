619e75cbbd277eacae20bffffcba1c7bf3f3ee02
kaneko <kaneko@macbookpro>
2023-01-06 14:53:27 +0900

    Basic implementation completed.

--- src/content_script.js
+++ src/content_script.js
@@ -1,11 +1,40 @@
-﻿//----------------------
-$(function () {
-    //↓疎通確認コード：タイトルの■が回転するはず
-    console.log('Chrome拡張の疎通確認コードが動いています。動作確認ができたら削除してください。');
-    let flag = false;
-    setInterval(function () {
-        document.title = (flag ? "■" : "◆") + document.title.replace(/^[■◆]/, '');
-        flag = !flag;
-    }, 1000);
-    //↑疎通確認コード
+﻿$(function () {
+  var debug = false;
+
+  // Correspondence between English representations of months and numbers
+  const monthMap = {
+    'January': '1',
+    'February': '2',
+    'March': '3',
+    'April': '4',
+    'May': '5',
+    'June': '6',
+    'July': '7',
+    'August': '8',
+    'September': '9',
+    'October': '10',
+    'November': '11',
+    'December': '12',
+    'Jan': '1',
+    'Feb': '2',
+    'Mar': '3',
+    'Apr': '4',
+    'Jun': '6',
+    'Jul': '7',
+    'Aug': '8',
+    'Sep': '9',
+    'Oct': '10',
+    'Nov': '11',
+    'Dec': '12'
+  };
+
+  // Create English representation of months using regular expression
+  const monthRegexSource = Object.keys(monthMap).map(month => `\\b${month}\\b`).join('|');
+  const regex = new RegExp(monthRegexSource, 'g');
+  if(debug)console.log(regex);
+
+  // Replace English representation of months with numbers
+  var text = document.body.innerHTML;
+  const newText = text.replace(regex, match => match + '<span>(' + monthMap[match] + ')</span>');
+  document.body.innerHTML = newText;
 });
