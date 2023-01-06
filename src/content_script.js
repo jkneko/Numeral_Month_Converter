c58ad320a7078151488c5bae420dc95b510fb8b3
kaneko <kaneko@>
2023-01-06 15:01:33 +0900

    大文字小文字を問わず置換するよう変更。不要なファイルを削除

--- src/content_script.js
+++ src/content_script.js
@@ -3,38 +3,39 @@
 
   // Correspondence between English representations of months and numbers
   const monthMap = {
-    'January': '1',
-    'February': '2',
-    'March': '3',
-    'April': '4',
-    'May': '5',
-    'June': '6',
-    'July': '7',
-    'August': '8',
-    'September': '9',
-    'October': '10',
-    'November': '11',
-    'December': '12',
-    'Jan': '1',
-    'Feb': '2',
-    'Mar': '3',
-    'Apr': '4',
-    'Jun': '6',
-    'Jul': '7',
-    'Aug': '8',
-    'Sep': '9',
-    'Oct': '10',
-    'Nov': '11',
-    'Dec': '12'
+    'january': '1',
+    'february': '2',
+    'march': '3',
+    'april': '4',
+    'may': '5',
+    'june': '6',
+    'july': '7',
+    'august': '8',
+    'september': '9',
+    'october': '10',
+    'november': '11',
+    'december': '12',
+    'jan': '1',
+    'feb': '2',
+    'mar': '3',
+    'apr': '4',
+    'may': '5',
+    'jun': '6',
+    'jul': '7',
+    'aug': '8',
+    'sep': '9',
+    'oct': '10',
+    'nov': '11',
+    'dec': '12'
   };
 
   // Create English representation of months using regular expression
   const monthRegexSource = Object.keys(monthMap).map(month => `\\b${month}\\b`).join('|');
-  const regex = new RegExp(monthRegexSource, 'g');
+  const regex = new RegExp(monthRegexSource, 'gi');
   if(debug)console.log(regex);
 
   // Replace English representation of months with numbers
   var text = document.body.innerHTML;
-  const newText = text.replace(regex, match => match + '<span>(' + monthMap[match] + ')</span>');
+  const newText = text.replace(regex, match => match + '<span>(' + monthMap[match.toLowerCase()] + ')</span>');
   document.body.innerHTML = newText;
 });
